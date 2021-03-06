9018f4cb48a4af802f3047928c7813650ce69eae
Osama-Elghazaly <oaelghazaly@alfaisal.edu>
2021-03-06 13:53:38 +0300

    testt

---
+++ app/src/main/java/com/example/se328lab5ex1/MoltenCake.java
@@ -0,0 +1,14 @@
+package com.example.se328lab5ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoltenCake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_molten_cake);
+    }
+}
