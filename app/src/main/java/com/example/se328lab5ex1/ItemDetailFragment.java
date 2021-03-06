67198064d9c6e1788203a5610394e92466d4c2bc
Osama-Elghazaly <oaelghazaly@alfaisal.edu>
2021-03-06 13:55:26 +0300

    testt

--- app/src/main/java/com/example/se328lab5ex1/ItemDetailFragment.java
+++ app/src/main/java/com/example/se328lab5ex1/ItemDetailFragment.java
@@ -16,7 +16,7 @@ import com.example.se328lab5ex1.dummy.DummyContent;
 
 /**
  * A fragment representing a single Item detail screen.
- * This fragment is either contained in a {@link ItemListActivity}
+ * This fragment iss either contained in a {@link ItemListActivity}
  * in two-pane mode (on tablets) or a {@link ItemDetailActivity}
  * on handsets.
  */
